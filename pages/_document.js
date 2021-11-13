b307b21455e28c194e3411a5bbec29498ebfa6e6
yusuf <yusufsaydullayev@gmail.com>
2021-11-13 17:26:16 +0500

    ok

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head/>
+
+        <body className='font-Poppins'>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
