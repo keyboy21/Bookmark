b1cc92beef3fa433c8870fd8ff32dfba8d1633c3
yusuf <yusufsaydullayev@gmail.com>
2021-11-19 20:32:52 +0500

    PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,30 @@
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
 module.exports = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        // This doesn't work for 'Cache-Control' key (works for others though):
+        source: "/_next/image(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            // Instead of this value:
+            value: "public, no-cache, no-store",
+            // Cache-Control response header is `public, max-age=60` in production
+            // and `public, max-age=0, must-revalidate` in development
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+});
