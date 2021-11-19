864df57583b1950bf1510b13a88edfb2c79aad57
yusuf <yusufsaydullayev@gmail.com>
2021-11-19 20:44:22 +0500

    pwa

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
           <meta name="keywords" content="nextjs, tailwindcss" />
           <meta name='application-name' content='Bookmark App' />
           <meta name='apple-mobile-web-app-capable' content='yes' />
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
+      <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
