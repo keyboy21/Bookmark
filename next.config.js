ebd540d0a5e288849abc0e843849b9c5b913872a
yusuf <yusufsaydullayev@gmail.com>
2021-11-24 19:47:37 +0500

    ignore

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   reactStrictMode: true,
@@ -22,6 +21,8 @@ module.exports = withPWA({
   },
   pwa: {
     dest: "public",
-    runtimeCaching,
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
   },
 });
