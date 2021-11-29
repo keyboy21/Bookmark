33dd07bb83be38f6934d28ba9cf4ccac7a2194c1
yusuf <yusufsaydullayev@gmail.com>
2021-11-29 21:20:34 +0500

    rem link

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,6 @@ export default function Home() {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
         />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
-          integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
         <title>Bookmark</title>
       </Head>
 
