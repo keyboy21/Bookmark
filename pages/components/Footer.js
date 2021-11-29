9936b8b83c4e6c9a5e55a32d50ed2dc79e0aab1d
yusuf <yusufsaydullayev@gmail.com>
2021-11-29 21:18:33 +0500

    icons

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -18,13 +18,27 @@ const Footer = () => {
           </ul>
         </div>
         <div className="flex gap-10 mt-12 md:mt-0">
-          
-          <li className="list-none cursor-pointer">
-            <i className="text-white text-2xl fab fa-twitter"></i>
-          </li>
-          <li className="list-none cursor-pointer">
-            <i className="text-white text-2xl fab fa-facebook-square"></i>
-          </li>
+          <i>
+            <svg
+              height="20"
+              viewBox="0 0 18 15"
+              aria-label="twitter"
+              className="fill-current text-2xl text-white"
+            >
+              <path d="M18 1.684l-1.687 1.684v.28c0 .307-.05.602-.123.886-.04 2.316-.777 5.387-3.816 7.81C6.404 17.115 0 12.907 0 12.907c5.063 0 5.063-1.684 5.063-1.684-1.126 0-3.376-2.243-3.376-2.243.563.56 1.689 0 1.689 0C.56 7.295.56 5.61.56 5.61c.563.561 1.689 0 1.689 0C-.563 3.368 1.124.561 1.124.561 1.687 3.368 9 4.49 9 4.49l.093-.046A6.637 6.637 0 0 1 9 3.368C9 1.353 10.636 0 12.656 0c1.112 0 2.094.506 2.765 1.286l.329-.163L17.437 0l-1.122 2.245L18 1.684z"></path>
+            </svg>
+          </i>
+
+          <i>
+            <svg
+              aria-label="facebook"
+              className="fill-current text-2xl text-white"
+              height="25"
+              viewBox="0 0 50 50"
+            >
+              <path d="M40,0H10C4.486,0,0,4.486,0,10v30c0,5.514,4.486,10,10,10h30c5.514,0,10-4.486,10-10V10C50,4.486,45.514,0,40,0z M39,17h-3 c-2.145,0-3,0.504-3,2v3h6l-1,6h-5v20h-7V28h-3v-6h3v-3c0-4.677,1.581-8,7-8c2.902,0,6,1,6,1V17z"></path>
+            </svg>
+          </i>
         </div>
       </div>
     </footer>
