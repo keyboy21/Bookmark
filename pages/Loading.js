f7f0e934a2166c05d5389f80fb999c1df91d7acf
yusuf <yusufsaydullayev@gmail.com>
2022-01-31 22:36:25 +0500

    dynmic import

---
+++ pages/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className='text-center'>
+     <div className="lds-circle"><div></div></div>
+    </div>
+  );
+}
+
+export default Loading;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Head from "next/head";
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Features from "./components/Features";
-import Downloads from "./components/Downloads";
-import Faq from "./components/Faq";
-import Contactus from "./components/Contactus";
-import Footer from "./components/Footer";
+import dynamic from "next/dynamic";
+// import Header from "./components/Header";
+// import Content from "./components/Content";
+// import Features from "./components/Features";
+// import Downloads from "./components/Downloads";
+// import Faq from "./components/Faq";
+// import Contactus from "./components/Contactus";
+// import Footer from "./components/Footer";
+
+const Loading = dynamic(() => import("./Loading"));
+const Header = dynamic(() => import("./components/Header"), {ssr: false, loading:Loading});
+const Features= dynamic(() => import("./components/Features"), {ssr: false,loading: Loading});
+const Content= dynamic(() => import("./components/Content"), {ssr: false,loading: Loading});
+const Faq= dynamic(() => import("./components/Faq"), {ssr: false,loading: Loading});
+const Downloads= dynamic(() => import("./components/Downloads"), {ssr: false,loading: Loading});
+const Contactus= dynamic(() => import("./components/Contactus"), {ssr: false,loading: Loading});
+const Footer= dynamic(() => import("./components/Footer"), {ssr: false,loading: Loading});
 
 export default function Home() {
   return (
