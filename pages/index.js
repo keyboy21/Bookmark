77c8a311e14f3fc55368db3acd32c184fd87711e
yusuf <yusufsaydullayev@gmail.com>
2022-02-01 20:50:06 +0500

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 import Head from "next/head";
 import dynamic from "next/dynamic";
-// import Header from "./components/Header";
-// import Content from "./components/Content";
-// import Features from "./components/Features";
-// import Downloads from "./components/Downloads";
-// import Faq from "./components/Faq";
-// import Contactus from "./components/Contactus";
-// import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Features from "./components/Features";
+import Downloads from "./components/Downloads";
+import Faq from "./components/Faq";
+import Contactus from "./components/Contactus";
+import Footer from "./components/Footer";
 
 // const Loading = dynamic(() => import("./Loading"));
-const Header = dynamic(() => import("./components/Header"), {ssr: false});
-const Features= dynamic(() => import("./components/Features"), {ssr: false});
-const Content= dynamic(() => import("./components/Content"), {ssr: false});
-const Faq= dynamic(() => import("./components/Faq"), {ssr: false});
-const Downloads= dynamic(() => import("./components/Downloads"), {ssr: false});
-const Contactus= dynamic(() => import("./components/Contactus"), {ssr: false});
-const Footer= dynamic(() => import("./components/Footer"), {ssr: false});
+// const Header = dynamic(() => import("./components/Header"), {ssr: false});
+// const Features= dynamic(() => import("./components/Features"), {ssr: false});
+// const Content= dynamic(() => import("./components/Content"), {ssr: false});
+// const Faq= dynamic(() => import("./components/Faq"), {ssr: false});
+// const Downloads= dynamic(() => import("./components/Downloads"), {ssr: false});
+// const Contactus= dynamic(() => import("./components/Contactus"), {ssr: false});
+// const Footer= dynamic(() => import("./components/Footer"), {ssr: false});
 
 export default function Home() {
   return (
