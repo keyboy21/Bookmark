b2c9b65ef494c69064674230ef3daeb540619cf6
Yusuf <yusufsaydullayev@gmail.com>
2022-11-29 21:28:52 +0500

    ok

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const prod = process.env.NODE_ENV === 'production'
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  skipWaiting: true,
   disable: prod ? false : true,
 })
 const nextConfig = {
@@ -34,37 +33,3 @@ const nextConfig = {
 }
 
 module.exports = withPWA(nextConfig)
-
-
-
-// const withPWA = require("next-pwa");
-
-// module.exports = withPWA({
-//   reactStrictMode: true,
-//   images: {
-//     domains: ['lh3.googleusercontent.com'],
-//   },
-//   async headers() {
-//     return [
-//       {
-//         // This doesn't work for 'Cache-Control' key (works for others though):
-//         source: "/_next/image(.*)",
-//         headers: [
-//           {
-//             key: "Cache-Control",
-//             // Instead of this value:
-//             value: "public, no-cache, no-store",
-//             // Cache-Control response header is `public, max-age=60` in production
-//             // and `public, max-age=0, must-revalidate` in development
-//           },
-//         ],
-//       },
-//     ];
-//   },
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     skipWaiting: true,
-//     disable: process.env.NODE_ENV === "development",
-//   },
-// });
