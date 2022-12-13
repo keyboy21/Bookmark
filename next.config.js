a8529b9faedc135e9a0a379b957558aa99931fe4
Yusuf <yusufsaydullayev@gmail.com>
2022-12-13 21:39:54 +0500

    bundle analyz

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@
 // @ts-ignore
 const prod = process.env.NODE_ENV === 'production'
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
@@ -38,3 +42,4 @@ const nextConfig = {
 }
 
 module.exports = withPWA(nextConfig)
+module.exports = withBundleAnalyzer({})
